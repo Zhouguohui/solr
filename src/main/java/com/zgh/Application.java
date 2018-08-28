9b19f33d1e86ea00c506b7466c7fc72f2d768a28
zgh <zgh1@163.com>
2018-08-28 22:44:55 +0800

    日志添加

--- src/main/java/com/zgh/Application.java
+++ src/main/java/com/zgh/Application.java
@@ -1,5 +1,8 @@
 package com.zgh;
 
+import org.apache.logging.log4j.spi.AbstractLogger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,10 +11,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * SpringBootApplication
  */
 
+
+
 @SpringBootApplication
 public class Application {
-
+    private  static final Logger logger =  LoggerFactory.getLogger(Application.class);
     public static void main(String[] args) {
+        logger.info("111111111111111");
+
         SpringApplication.run(Application.class, args);
     }
 }
--- src/test/java/com/zgh/ApplicationTests.java
+++ src/test/java/com/zgh/ApplicationTests.java
@@ -2,6 +2,8 @@ package com.zgh;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -11,12 +13,15 @@ import com.zgh.springboot.entity.User;
 @SpringBootTest
 public class ApplicationTests {
 
+    private static final Logger logger = LoggerFactory.getLogger(ApplicationTests.class);
     @Autowired
     User user;
 
     @Test
     public void contextLoads() {
+        logger.info("11111111");
         System.out.println(user.toString());
+        logger.info("22222222");
     }
 
 }
