0c0c66ad8b3d8bec066893924faa077c5bd9c003
zgh <zgh1@163.com>
2018-08-29 23:18:51 +0800

    删除无用的东西

--- src/main/java/com/zgh/Application.java
+++ src/main/java/com/zgh/Application.java
@@ -17,8 +17,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     private  static final Logger logger =  LoggerFactory.getLogger(Application.class);
     public static void main(String[] args) {
-        logger.info("111111111111111");
-
         SpringApplication.run(Application.class, args);
     }
 }
