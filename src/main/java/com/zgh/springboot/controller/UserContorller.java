b6cb419ff478fe90234e697186dfe0dcfe02798b
zgh <zgh1@163.com>
2018-08-29 23:36:00 +0800

    修改问题

--- src/main/java/com/zgh/springboot/controller/UserContorller.java
+++ src/main/java/com/zgh/springboot/controller/UserContorller.java
@@ -2,18 +2,24 @@ package com.zgh.springboot.controller;
 
 
 import com.zgh.springboot.entity.Users;
+import com.zgh.springboot.service.DeptService;
 import com.zgh.springboot.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
+@Transactional
 public class UserContorller {
 
     @Autowired
     UsersService usersService;
+
+    @Autowired
+    DeptService deptService;
 
     @RequestMapping("/getUsers/{id}")
     public Users getUsers(@PathVariable("id") Integer id) {
@@ -23,11 +29,15 @@ public class UserContorller {
 
     @RequestMapping("deleteUsersById")
     public int deleteUsersById(Integer id) {
-        return usersService.deleteUsersById(id);
+        usersService.deleteUsersById(id);
+        deptService.deleteDeptById(id);
+      int j = 1/0;
+        return j;
     }
 
     @RequestMapping("insertUser")
     public int insertUser(Users users) {
+
         return usersService.insertUsers(users);
     }
 
